00b0849837ba30c73805ebf378f0b7126501dba1
LucasDemartini <Lucas.demartini@institutoivoti.com.br>
2017-09-19 21:17:54 -0300

    Criação do Projeto

---
+++ src/br/com/herenca/heranca.java
@@ -0,0 +1,17 @@
+package br.com.herenca;
+
+public class heranca {
+
+	public static void main(String[] args) {
+		System.out.println("EMPREGO");
+	}
+	
+	class Funcionario {
+		String nome;
+		String cpf;
+		double salario;
+	}
+	
+	
+	}
+
